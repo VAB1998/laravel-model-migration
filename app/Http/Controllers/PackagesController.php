8a50d041618ae216496d2403e280b8bdb8783d89
VAB1998 <blanco.vincenzo@outlook.com>
2021-11-04 16:31:45 +0100

    Retrieved datas from 'packages ' Table

--- app/Http/Controllers/PackagesController.php
+++ app/Http/Controllers/PackagesController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Package;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class PackagesController extends Controller
 {
+    
     public function index(){
-        return view('packages');
+
+        $packages = Package::all();
+
+        dd($packages);
+
+        return view('packages', [ 
+            'packages' => $packages,
+        ]);
     }
 }
