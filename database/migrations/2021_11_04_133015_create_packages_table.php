a5f71545b8f0ebc9322d25c7f201a744d59686a4
VAB1998 <blanco.vincenzo@outlook.com>
2021-11-04 15:37:49 +0100

    Created a 'packages' table for the 'travel_company' Database

---
+++ database/migrations/2021_11_04_133015_create_packages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('destination', 24);
+            $table->integer('number_of_adults');
+            $table->integer('number_of_children')->nullable();
+            $table->char('departure', 10);
+            $table->char('return', 10);
+            $table->boolean('is_avalaible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
